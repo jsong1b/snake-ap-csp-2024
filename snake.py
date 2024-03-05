0b8944cb0c4d342e8f38e225c2c94b7141788d95
Guieb, Jason <141759463+jas0n-g@users.noreply.github.com>
2024-03-05 01:01:10 +0000

    added drawing

--- snake.py
+++ snake.py
@@ -3,6 +3,7 @@
 import curses
 import sys
 
+
 def main(argv):
     if not len(argv) == 2:
         usage()
@@ -18,19 +19,21 @@ def main(argv):
         exit(2)
 
     board = createBoard(width, height)
-    if board == None:
+    if board == False:
         usage()
         exit(3)
+
+    curses.wrapper(snake, board)
 
     exit(0)
 
 def usage():
-    print("USAGE: ./snake.py [WIDTH] [HEIGHT]")
-    print("where [WIDTH] and [HEIGHT] are integers between 10 and 30")
+    print("USAGE: ./snake.py [HEIGHT] [WIDTH]")
+    print("where [HEIGHT] and [WIDTH] are integers between 10 and 30")
 
 def createBoard(width, height):
     if not 10 <= width <= 30 or not 10 <= height <= 30:
-        return None
+        return False
 
     board = []
     for i in range(height + 2):
@@ -49,6 +52,41 @@ def createBoard(width, height):
     board[height + 1][width + 1] = "+"
 
     return board
+
+
+def snake(stdscr, board):
+    height = len(board)
+    width = len(board[0])
+
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+
+    curses.curs_set(False)
+
+    snake = [(height // 2, width // 4), (height // 2, (width // 4) - 1)]
+    fruit = ((height // 2), width - (width // 4))
+
+    drawScreen(board, snake, fruit, stdscr)
+
+    stdscr.getstr()
+
+
+def drawScreen(board, snake, fruit, stdscr):
+    for i, _ in enumerate(board):
+        for j, _ in enumerate(board[i]):
+            if board[i][j] == "0":
+                continue
+            stdscr.addstr(i, j, board[i][j])
+
+    for i, pos in enumerate(snake):
+        stdscr.addstr(pos[0], pos[1], "O")
+        if i == 0:
+            stdscr.addstr(pos[0], pos[1], "O", curses.color_pair(1))
+
+    stdscr.addstr(fruit[0], fruit[1], "X")
+
+    stdscr.refresh()
 
 
 if __name__ == "__main__":
