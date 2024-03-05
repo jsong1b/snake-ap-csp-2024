06b67cf7449fcfc3269c8c3460f69bf317770ae8
Guieb, Jason <141759463+jas0n-g@users.noreply.github.com>
2024-03-05 18:55:22 +0000

    fixed errors

--- snake.py
+++ snake.py
@@ -6,9 +6,11 @@ import sys
 import time
 import random
 
+
 ##################
 # INITIALIZATION #
 ##################
+
 
 def main(argv):
     if not len(argv) == 2:
@@ -29,16 +31,21 @@ def main(argv):
         usage()
         exit(3)
 
-    curses.wrapper(snake, board)
+    try:
+        curses.wrapper(snake, board)
+    except Exception as err:
+        print(err)
 
     exit(0)
 
+
 def usage():
-    print("USAGE: ./snake.py [HEIGHT] [WIDTH]")
-    print("where [HEIGHT] and [WIDTH] are integers between 10 and 50")
+    print("USAGE: ./snake.py [WIDTH] [HEIGHT]")
+    print("where [HEIGHT] and [WIDTH] are integers between 10 and the bounds of your terminal")
+
 
 def createBoard(width, height):
-    if not 10 <= width <= 50 or not 10 <= height <= 50:
+    if width < 10 or height < 10:
         return False
 
     board = []
@@ -69,6 +76,12 @@ def snake(stdscr, board):
     height = len(board)
     width = len(board[0])
 
+    maxHeight = curses.LINES - 3
+    maxWidth = curses.COLS - 1
+
+    if height >= maxHeight or width >= maxWidth:
+        raise Exception("Bounds too large, the max width is " + str(maxWidth - 3) + " and the max height is " + str(maxHeight - 3))
+
     curses.start_color()
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)
@@ -84,30 +97,28 @@ def snake(stdscr, board):
         snake.append([height // 2, (width // 4) - i])
     fruit = [(height // 2), width - (width // 4)]
 
-    drawScreen(board, snake, fruit, 0, stdscr, direction)
+    drawScreen(board, snake, fruit, stdscr)
 
     stdscr.addstr(height + 2, 0, "Press any key to start")
     stdscr.refresh()
     stdscr.getch()
     stdscr.addstr(height + 2, 0, "")
 
-    score = 0
-    drawScreen(board, snake, fruit, score, stdscr, direction)
+    drawScreen(board, snake, fruit, stdscr)
 
     while True:
-        direction = getDirection(direction, stdscr)
         time.sleep(0.1)
 
         direction = getDirection(direction, stdscr)
-        drawScreen(board, snake, fruit, score, stdscr, direction)
-        if updateGame(board, snake, fruit, score, direction) == False:
+        drawScreen(board, snake, fruit, stdscr)
+        if updateGame(board, snake, fruit, direction) == False:
             break
 
         if snake[0][0] == 0 or snake[0][1] == 0 or snake[0][0] == len(board) - 1 or snake[0][1] == len(board[0]) - 1:
             break
 
     stdscr.nodelay(0)
-    stdscr.addstr(height + 2, 0, "GAME OVER - SCORE: " + str(score), curses.A_BOLD | curses.A_UNDERLINE)
+    stdscr.addstr(height + 2, 0, "GAME OVER - SCORE: " + str(len(snake) - 3), curses.A_BOLD | curses.A_UNDERLINE)
     stdscr.addstr(height + 3, 0, "Press any key to exit")
     stdscr.refresh()
     time.sleep(0.25)
@@ -115,11 +126,11 @@ def snake(stdscr, board):
     stdscr.getch()
 
 
-def drawScreen(board, snake, fruit, score, stdscr, direction):
+def drawScreen(board, snake, fruit, stdscr):
     stdscr.clear()
 
-    for i, _ in enumerate(board):
-        for j, _ in enumerate(board[i]):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
             stdscr.addstr(i, j, board[i][j])
 
     for i, pos in enumerate(snake):
@@ -129,12 +140,12 @@ def drawScreen(board, snake, fruit, score, stdscr, direction):
 
     stdscr.addstr(fruit[0], fruit[1], " ", curses.color_pair(2) | curses.A_BOLD)
 
-    stdscr.addstr(len(board) + 2, 0, "SCORE: " + str(score))
+    stdscr.addstr(len(board) + 2, 0, "SCORE: " + str(len(snake) - 3))
 
     stdscr.refresh()
 
 
-def updateGame(board, snake, fruit, score, direction):
+def updateGame(board, snake, fruit, direction):
     oldSnake = copy.deepcopy(snake)
 
     if direction == "right":
@@ -150,7 +161,6 @@ def updateGame(board, snake, fruit, score, direction):
         snake[i] = oldSnake[i - 1]
 
     if snake[0] == fruit:
-        score += 1
         snake.append(oldSnake[-1])
         fruit[:] = list(newFruit(snake, fruit, board))
 
@@ -199,5 +209,6 @@ def newFruit(snake, fruit, board):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
 
 # ex: set nu relativenumber list :
